7cf93867e1bcca78f95b3203eddeeeb28a530c07
hyq07508 <hyq07508@ly.com>
2021-04-22 19:04:13 +0800

    登录重定向接口

--- src/main/java/com/hyq/baseadmin/controller/AuthenticationController.java
+++ src/main/java/com/hyq/baseadmin/controller/AuthenticationController.java
@@ -3,23 +3,34 @@ package com.hyq.baseadmin.controller;
 import com.hyq.baseadmin.bean.UserBean;
 import com.hyq.baseadmin.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by hyq on 2021/4/21 22:14.
  */
-@RestController
+@Controller
 @RequestMapping("/auth")
 public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@RequestBody UserBean user) {
+    @RequestMapping(value = "/getToken", method = RequestMethod.POST)
+    @ResponseBody
+    public String getToken(@RequestBody UserBean user) {
 
         return authenticationService.getToken(user);
+    }
+
+    @RequestMapping(value = "/login")
+    public String login(HttpServletRequest request, HttpServletResponse response) {
+        return "redirect:http://localhost:3000";
+//        return "redirect:http://baidu.com";
     }
 }
--- src/main/java/com/hyq/baseadmin/controller/TestController.java
+++ src/main/java/com/hyq/baseadmin/controller/TestController.java
@@ -1,16 +1,11 @@
 package com.hyq.baseadmin.controller;
 
-import com.hyq.baseadmin.bean.UserBean;
 import com.hyq.baseadmin.mapper.UserMapper;
 import com.hyq.baseadmin.mapper.plus.UserBeanMapper;
-import com.hyq.baseadmin.service.AuthenticationService;
-import org.apache.catalina.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * Created by hyq on 2021/4/8 10:26.
